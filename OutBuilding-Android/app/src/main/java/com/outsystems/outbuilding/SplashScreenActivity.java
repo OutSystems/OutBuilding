ee1f4b86c26798ead23a3514d4c2537b8e0d54bc
Bruno Grácio <bruno.gracio@outsystems.com>
2017-01-18 11:28:17 +0000

    Android: Added SplashscreenActivity and MainActivity

--- OutBuilding-Android/app/src/main/java/com/outsystems/outbuilding/SplashScreenActivity.java
+++ OutBuilding-Android/app/src/main/java/com/outsystems/outbuilding/SplashScreenActivity.java
@@ -1,13 +1,33 @@
 package com.outsystems.outbuilding;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private static final int splashscreenDelay = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        delaySplashScreen();
+    }
+
+    private void delaySplashScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goNextActivity();
+            }
+        }, splashscreenDelay);
+    }
+
+    protected void goNextActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
