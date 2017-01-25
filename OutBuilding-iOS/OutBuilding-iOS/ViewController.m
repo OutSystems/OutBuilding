b6e77b9b37ae251f93ef8a2131751bec437ab4e9
Bruno Grácio <bruno.gracio@outsystems.com>
2017-01-25 11:41:32 +0000

    iOS: Zoom and Pan

--- OutBuilding-iOS/OutBuilding-iOS/ViewController.m
+++ OutBuilding-iOS/OutBuilding-iOS/ViewController.m
@@ -14,15 +14,33 @@
 
 @implementation ViewController
 
+@synthesize imageView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // This could be in the storyboard!
+    UIImage *image = [UIImage imageNamed:@"vr_troopers.jpg"];
+    imageView = [[UIImageView alloc] initWithImage:image];
+    [self.view addSubview:imageView];
+    
+    // We need this to say to the view the size of the image we want to display
+    [(UIScrollView *)self.view setContentSize: [image size]];
+    
+    // Set the zoom scale
+    [(UIScrollView *)self.view setMaximumZoomScale:3.0];
+    [(UIScrollView *)self.view setMinimumZoomScale:1.0];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// This method tells the view what image we are trying to zoom
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return imageView;
 }
 
 @end
