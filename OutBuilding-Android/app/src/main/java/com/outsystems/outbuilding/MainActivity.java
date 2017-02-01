1aaa77effda15b6daedcbd1ebc16643d7b497e49
Bruno Grácio <bruno.gracio@outsystems.com>
2017-02-01 12:17:18 +0000

    Android: Added black background view and auto complete

--- OutBuilding-Android/app/src/main/java/com/outsystems/outbuilding/MainActivity.java
+++ OutBuilding-Android/app/src/main/java/com/outsystems/outbuilding/MainActivity.java
@@ -2,8 +2,12 @@ package com.outsystems.outbuilding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
 
         // Create the adapter and set it to the AutoCompleteTextView
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, people);
+
+        // Add adapter to AutoCompleteTextView
         autoCompleteTextView.setAdapter(adapter);
+
+        autoCompleteTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("AUTO_COMPLETE", "Click");
+
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.blackBackground);
+                linearLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Log.d("AUTO_COMPLETE", "Item position: " + position);
+                Log.d("AUTO_COMPLETE", "Item Value: " + parent.getItemAtPosition(position));
+
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.blackBackground);
+                linearLayout.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
