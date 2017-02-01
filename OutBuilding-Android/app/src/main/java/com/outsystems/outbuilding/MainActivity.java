a602e7a018cca0ed98954f53115092cbb4edfd71
Bruno Grácio <bruno.gracio@outsystems.com>
2017-02-01 11:38:03 +0000

    Android: New Where to Field

--- OutBuilding-Android/app/src/main/java/com/outsystems/outbuilding/MainActivity.java
+++ OutBuilding-Android/app/src/main/java/com/outsystems/outbuilding/MainActivity.java
@@ -2,8 +2,12 @@ package com.outsystems.outbuilding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] people = {"Cesar Afonso", "Bruno Grácio", "João Ferreira", "Carlos Simões", "Calhau", "Calhordas", "CaCenas"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the image view so we can use for overlays
         //TouchImageView touchImg = (TouchImageView) findViewById(R.id.touchImageView);
+
+        // Get a reference to the AutoCompleteTextView in the layout
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+
+        // Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, people);
+        autoCompleteTextView.setAdapter(adapter);
     }
 }
